242fae016d8809a2da3ce588af32754c8ce13906
Alli Ibrahim <iballi2017@gmail.com>
2018-09-26 21:38:20 -0700

    edited

---
+++ alli--homepage.js
@@ -0,0 +1,42 @@
+
+// I hope in the future, i can be able to write the following codes in a shorter form...lol
+        // The following JavaScript codes reveals captions when mouse is palced on the image
+        function homepageShow(){
+            document.getElementById("plant").style.display="block";
+        }
+        function aboutpageShow(){
+            document.getElementById("aboutpage").style.display="block";
+        }
+        function volkpageShow(){
+            document.getElementById("volkpage").style.display="block";
+        }
+        function mobilepage1Show(){
+            document.getElementById("mobilepage-profile").style.display="block";
+        }
+        function mobilepage2Show(){
+            document.getElementById("mobilepage-news").style.display="block";
+        }
+        function wanderlustShow(){
+            document.getElementById("wanderlust-caption").style.display="block";
+        }
+
+        // The following JavaScript codes hides captions when mouse moves away from the image
+
+        function homepageHide(){
+            document.getElementById("homepage").style.display="none";
+        }
+        function aboutpageHide(){
+            document.getElementById("aboutpage").style.display="none";
+        }
+        function volkpageHide(){
+            document.getElementById("volkpage").style.display="none";
+        }
+        function mobilepage1Hide(){
+            document.getElementById("mobilepage-profile").style.display="none";
+        }
+        function mobilepage2Hide(){
+            document.getElementById("mobilepage-news").style.display="none";
+        }
+        function wanderlustHide(){
+            document.getElementById("wanderlust-caption").style.display="none";
+        }
